1d5732dae0a74424a5706646d31ff0c7fe669ef0
kritzerenkrieg <kentaro.keren@gmail.com>
2023-06-06 15:40:51 +0700

    landing kontrol

--- rbpl/app/Http/Controllers/LandingController.php
+++ rbpl/app/Http/Controllers/LandingController.php
@@ -7,13 +7,11 @@ use Illuminate\Http\Request;
 class LandingController extends Controller
 {
 
-<<<<<<< HEAD
     public function landing(){
         return view('landing');
-=======
+        
     public function dashboard(){
         return view('stock');
->>>>>>> 17044ed4d01ab499612273b0f59deaeca5addb9e
     }
     //
 }
