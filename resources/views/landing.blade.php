c6ed381eb6dde914d0e4d14f20a060bfd2fff504
kritzerenkrieg <kentaro.keren@gmail.com>
2023-06-13 20:55:11 +0700

    landing v2

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -8,46 +8,87 @@
         <img src="{{ asset('images/landing.jpg') }}" class="img-fluid no-space" alt="Image Missing">
   </div>
 
-  <div class="container-2 d-grid">
+  <div class="container-2 d-grid card-header">
           <div class="title text-left d-flex flex-column p-4">
             <h2 class="main-title">Gudang menarik untuk bisnismu</h2>
             <p class="sub-title">Ada beragam jenis gudang yang cocok buat kamu nih! Yuk temukan selera hatimu sekarang!</p>
-          </div>
-  </div>
-
-  <div class="container">
-        <div class="text-center mt-5 mb-5 d-grid gap-2">
-          <button class="btn zrayastyle">
+            <p><p><p><p>  {{--INI NANTI DELETE KLO ADA PAGINATE--}}
+            <div class="zrayacenter">
+            <button class="btn zrayastyle">
             Lihat Semua Gudang
-          </button>
-        </div>
+            </button>
+            </div>
+        
+          </div>
+
+          
   </div>
 
-  <div class="container-2 d-grid">
+  <div class="container-2 d-grid card-header">
           <div class="title text-left d-flex flex-column p-4">
-            <h2 class="main-title">Kenapa kamu harus menggunakan jasa gudang Relow?</h2>
+            <h2 class="main-title mb-4">Kenapa kamu harus menggunakan jasa gudang Relow?</h2>
+
+            <div id="accordion">
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed btn card-font" data-bs-toggle="collapse" href="#collapseOne">
+          Proses Cepat
+        </a>
+      </div>
+      <div id="collapseOne" class="collapse" data-bs-parent="#accordion">
+        <div class="card-body sub-title">
+        Jasa kami memiliki proses yang cepat karena kami mengutamakan efisiensi dan kecepatan dalam memberikan layanan kepada pelanggan kami. Pertama, kami memiliki tim yang terdiri dari para ahli yang terlatih dengan baik di bidang mereka masing-masing. Mereka memiliki pengetahuan dan keterampilan yang mendalam dalam menjalankan tugas-tugas yang diberikan. Hal ini memungkinkan mereka untuk bekerja dengan cepat dan efektif, tanpa mengorbankan kualitas pekerjaan.
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed btn btn card-font" data-bs-toggle="collapse" href="#collapseTwo">
+        Biaya Terjangkau
+      </a>
+      </div>
+      <div id="collapseTwo" class="collapse" data-bs-parent="#accordion">
+        <div class="card-body sub-title">
+        Biaya kami terjangkau karena kami berkomitmen untuk memberikan nilai terbaik kepada pelanggan kami tanpa memberatkan kantong mereka. Pertama, kami memiliki kebijakan harga yang transparan dan adil. Kami menyadari bahwa setiap pelanggan memiliki anggaran yang berbeda, dan oleh karena itu, kami menawarkan paket harga yang fleksibel dan disesuaikan dengan kebutuhan individu. Kami tidak menambahkan biaya tersembunyi atau biaya tambahan yang tidak perlu, sehingga pelanggan dapat memiliki gambaran yang jelas tentang apa yang mereka bayar.
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed btn card-font" data-bs-toggle="collapse" href="#collapseThree">
+          Lokasi Tersebar
+        </a>
+      </div>
+      <div id="collapseThree" class="collapse" data-bs-parent="#accordion">
+        <div class="card-body sub-title">
+        Lokasi layanan kami tersebar karena kami ingin memberikan aksesibilitas yang lebih baik kepada pelanggan di berbagai daerah. Kami percaya bahwa setiap pelanggan memiliki kebutuhan unik dan tidak semua orang dapat dengan mudah mengakses layanan di satu titik sentral. Dengan memiliki lokasi layanan yang tersebar, kami dapat memberikan kenyamanan dan kemudahan bagi pelanggan kami.
+        </div>
+      </div>
+    </div>
+  </div>
+{{--
           <div class="container p-2 mt-2 d-grid gap-2">
             <button class="btn zrayastyle" data-bs-toggle="collapse" data-bs-target="#demo1">Proses Cepat</button>
-              <div id="demo1" class="collapse">
-              AAAAA ZORAAAAAAAAAAAA
+              <div id="demo1" class="collapse sub-title">
+              Jasa kami memiliki proses yang cepat karena kami mengutamakan efisiensi dan kecepatan dalam memberikan layanan kepada pelanggan kami. Pertama, kami memiliki tim yang terdiri dari para ahli yang terlatih dengan baik di bidang mereka masing-masing. Mereka memiliki pengetahuan dan keterampilan yang mendalam dalam menjalankan tugas-tugas yang diberikan. Hal ini memungkinkan mereka untuk bekerja dengan cepat dan efektif, tanpa mengorbankan kualitas pekerjaan.
               </div>
           </div>
           <div class="container p-2 mt-2 d-grid gap-2">
             <button class="btn zrayastyle" data-bs-toggle="collapse" data-bs-target="#demo2">Biaya Terjangkau</button>
-              <div id="demo2" class="collapse">
-              CUUUUUUUUUTEEEE AND FUNNNYYYYYYY
+              <div id="demo2" class="collapse sub-title">
+              Biaya kami terjangkau karena kami berkomitmen untuk memberikan nilai terbaik kepada pelanggan kami tanpa memberatkan kantong mereka. Pertama, kami memiliki kebijakan harga yang transparan dan adil. Kami menyadari bahwa setiap pelanggan memiliki anggaran yang berbeda, dan oleh karena itu, kami menawarkan paket harga yang fleksibel dan disesuaikan dengan kebutuhan individu. Kami tidak menambahkan biaya tersembunyi atau biaya tambahan yang tidak perlu, sehingga pelanggan dapat memiliki gambaran yang jelas tentang apa yang mereka bayar.
               </div>
           </div>
           <div class="container p-2 mt-2 d-grid gap-2">
             <button class="btn zrayastyle" data-bs-toggle="collapse" data-bs-target="#demo3">Lokasi Tersebar</button>
-              <div id="demo3" class="collapse">
-              SEPERTI CINTA KU PADA MUUUUU
+              <div id="demo3" class="collapse sub-title">
+              Lokasi layanan kami tersebar karena kami ingin memberikan aksesibilitas yang lebih baik kepada pelanggan di berbagai daerah. Kami percaya bahwa setiap pelanggan memiliki kebutuhan unik dan tidak semua orang dapat dengan mudah mengakses layanan di satu titik sentral. Dengan memiliki lokasi layanan yang tersebar, kami dapat memberikan kenyamanan dan kemudahan bagi pelanggan kami.
               </div>
           </div>
       </div>
   </div>
-
-  <div class="container-2">
+--}}
+  <div class="container-2 card-header">
           <div class="title text-left mt-5 d-flex flex-column p-4">
             <h2 class="main-title">Panduan lengkap untuk menggunakan jasa gudang Relow</h2>
           </div>
