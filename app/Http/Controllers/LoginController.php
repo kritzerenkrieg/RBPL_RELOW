4cb3e7690e5ffac5381a8a3c7be5aadb27ab9fd5
Ilhammzz <ilham.abdulaziz20@gmail.com>
2023-06-18 03:03:27 +0700

    y

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -44,7 +44,7 @@ class LoginController extends Controller
 
 		if(Auth::attempt($credentials)) {
 			$request->session()->regenerate();
-			return redirect()->intended('dashboard');
+			return redirect()->intended('landing');
 		}
 
 		return back()->with('loginError', 'Login gagal');
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -35,7 +35,7 @@
                     </a>
                 </div>
                 <div class="nav-link text-center w-100">
-                    <a href="{{url('/forgot-password')}}" class="nv active">
+                    <a href="{{url('/dashboard')}}" class="nv active">
 
                         <span class="material-icons">account_circle</span>
                         <p class="nav-text-title">Dashboard</p>
